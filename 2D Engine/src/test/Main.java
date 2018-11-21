f5661ca403b3270715dc8a6b5f7a035cb98c08d5
paulz <paulz@DESKTOP-JQEAA2G.local>
2018-11-21 23:42:10 +0100

    a

--- 2D Engine/src/test/Main.java
+++ 2D Engine/src/test/Main.java
@@ -3,6 +3,7 @@ package test;
 import java.awt.Color;
 
 import base.Base;
+import base.utility.maths.Maths;
 import base.utility.maths.Vector;
 import base.world.entitys.BasicShapedEntity;
 import base.world.entitys.MovingEntity;
@@ -11,14 +12,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		Base.initialize("TEST221",new Vector(800,500));
+		System.out.println("aaa");
+		for(int i = 0; i < 200; i++) {
 		Base.getWorld().addEntity(
 				new MovingEntity(
 						new Vector(100,100), 
 						new Vector(20,50),
 						Color.GREEN,
 						BasicShapedEntity.FILL_RECT,
-						new Vector(150,150),
+						new Vector(Maths.random(-150, 150),Maths.random(-150, 150)),
 						MovingEntity.REFLECT_AT_BORDER));
+		}
 		Base.startLoop();
 	}
 }
